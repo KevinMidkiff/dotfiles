e5d062da6a6a4846a0169277ec45e404451563c8
Kevin Midkiff <midkiff.km@gmail.com>
2022-06-02 16:21:33 -0700

    Add Telescope

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -1,6 +1,6 @@
 local opts = { noremap = true, silent = true }
 
-local term_opts = {silent = true }
+local term_opts = { silent = true }
 
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
@@ -40,3 +40,9 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 -- Visual --
 
 -- Terminal --
+
+-- Telescope --
+-- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
+keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
+keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
+
