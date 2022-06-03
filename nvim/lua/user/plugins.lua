1246243a7f22ca619ac2622cd34f111b959ec00f
Kevin Midkiff <midkiff.km@gmail.com>
2022-06-03 08:05:25 -0700

    Fix treesitter configuration

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -72,8 +72,7 @@ return packer.startup(function(use)
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
   }
-  -- use "p00f/nvim-ts-rainbow"
-  -- use "nvim-treesitter/playground"
+  use "p00f/nvim-ts-rainbow"
 
   -- Git
   use "lewis6991/gitsigns.nvim"
