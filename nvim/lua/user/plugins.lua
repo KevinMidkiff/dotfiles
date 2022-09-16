1afe10773401c385c2a596ba5262da0b745a8cc7
Kevin Midkiff <midkiff.km@gmail.com>
2022-09-16 13:05:38 -0700

    Updates to neovim configuration to work with latest versions of plugins

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -92,6 +92,8 @@ return packer.startup(function(use)
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
 
+  use {'iamcco/markdown-preview.nvim'}
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
