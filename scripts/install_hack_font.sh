ef71f5baacf463ab0dd8e8c425af54d3ba464606
Kevin Midkiff <midkiff.km@gmail.com>
2023-09-18 14:24:15 -0700

    Add script updates

--- scripts/install_hack_font.sh
+++ scripts/install_hack_font.sh
@@ -1,32 +1,6 @@
 #!/bin/bash
 
-RED='\033[0;31m'
-YELLOW="\033[1;33m"
-GREEN="\033[0;32m"
-NC='\033[0m' # No Color
-
-function log_warn() {
-    echo -e "${YELLOW}WARN: $1 ${NC}"
-}
-
-function log_info() {
-    echo -e "${GREEN}INFO: $1 ${NC}"
-}
-
-function log_error() {
-    echo -e "${RED}ERROR: $1 ${NC}"
-}
-
-function log_fatal() {
-    echo -e "${RED}FATAL: $1 ${NC}"
-    exit -1
-}
-
-function check_error() {
-    if [ $? -ne 0 ] ; then
-        log_fatal "$1"
-    fi
-}
+source ${PWD}/scripts/utils.sh
 
 base_url="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts"
 font_filename="HackNerdFont-Regular.ttf"
@@ -34,7 +8,7 @@ font_path="Hack/Regular"
 
 uname_out="$(uname -s)"
 
-case "${uname_out}" in 
+case "${uname_out}" in
     Linux*)  machine=Linux;;
     Darwin*) machine=Darwin;;
     *)
@@ -47,26 +21,26 @@ if [[ "${machine}" -eq "Linux" ]] ; then
         mkdir -p ~/.local/share/fonts
         check_error "Failed to create fonts directory"
     fi
-        
-    cd ~/.local/share/fonts 
+
+    cd ~/.local/share/fonts
     check_error "Failed to go to honts directory"
 
     log_info "Downloading font: ${font_filename}"
     curl -fLo \
         "${font_filename}" \
-        ${base_url}/${font_path}/${font_filename} 
+        ${base_url}/${font_path}/${font_filename}
     check_error "Failed to download font"
 
     log_info "Reloading font cache"
     fc-cache -f -v
     check_error "Failed to rebuild font cache"
 else
-    cd ~/Library/Fonts 
+    cd ~/Library/Fonts
     check_error "Failed to go to ~/Library/Fonts"
-    
+
     log_info "Downloading font: ${font_filename}"
     curl -fLo \
         "${font_filename}" \
-        ${base_url}/${font_path}/${font_filename} 
+        ${base_url}/${font_path}/${font_filename}
     check_error "Failed to download font"
 fi
--- scripts/utils.sh
+++ scripts/utils.sh
@@ -4,6 +4,12 @@
 ## Various bash utility functions
 ##
 
+# Various helpful colors
+RED='\033[0;31m'
+YELLOW="\033[1;33m"
+GREEN="\033[0;32m"
+NC='\033[0m' # No Color
+
 function verify_not_root() {
     if [[ $EUID -eq 0 ]]; then
         echo -e "${RED}!!! ERROR: Should not be started as root${NC}" 1>&2
@@ -31,5 +37,30 @@ function log_fatal() {
 function check_error() {
     if [ $? -ne 0 ] ; then
         log_fatal "$1"
+    fi
+}
+
+function assert-linux() {
+    uname_out="$(uname -s)"
+    echo ${uname_out}
+    if [[ "${uname_out}" =~ "Linux"* ]] ; then
+        log_info "Running on Linux"
+    else
+        log_fatal "Not running on Linux"
+    fi
+}
+
+function assert-distro() {
+    # First verify running on Linux
+    assert-linux
+
+    # Verify lsb_release
+    lsb_release -a >/dev/null &2>/dev/null
+    check_error "Missing 'lsb_release'"
+
+    expected="$1"
+    distro_id=$(lsb_release -s -i)
+    if [ "${distro_id}" != "${expected}" ] ; then
+        log_fatal "Incorrect distro - got '${distro_id}', expected '${expected}'"
     fi
 }
