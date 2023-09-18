50d1a66d6c709507f5391de7e9fbf7984dc98a36
Kevin Midkiff <midkiff.km@gmail.com>
2023-09-18 14:37:28 -0700

    Add neovim installation on Ubuntu

--- packages/ubuntu/install.sh
+++ packages/ubuntu/install.sh
@@ -24,4 +24,42 @@ if [[ "${PIPESTATUS[1]}" -ne 0 ]] ; then
     log_fatal "Failed to install Ubuntu packages"
 fi
 
-# TODO(kmidkiff): Install neovim
+nvim_version=$(cat ${PWD}/nvim-version)
+nvim_url="https://github.com/neovim/neovim/releases/download/${nvim_version}/nvim-linux64.tar.gz"
+install_location=/opt/nvim-linux64/
+
+if [ -d "${install_location}" ] ; then
+    log_info "neovim already installed"
+else
+    neovim_tarfile=${PWD}/nvim-linux64.tar.gz
+    if [ -f "${neovim_tarfile}" ] ; then
+        rm ${neovim_tarfile}
+        check_error "Failed to delete previous download"
+    fi
+
+    log_info "Downloading NeoVIM version ${nvim_version}"
+    wget ${nvim_url}
+    check_error "Failed to download NeoVIM"
+
+    cwd=${PWD}
+    cd /opt/
+    check_error "Failed to go to /opt/ directory"
+
+    log_info "Extracting neovim tarfile to /opt/"
+    sudo tar xvf ${neovim_tarfile}
+    check_error "Failed to extract neovim"
+
+    log_info "Creating neovim binary soft-links to /usr/local/"
+
+    sudo ln -sf /opt/nvim-linux64/bin/* /usr/local/bin/
+    check_error "Failed to create soft-links for neovim binary"
+
+    sudo ln -sf /opt/nvim-linux64/lib/* /usr/local/lib/
+    check_error "Failed to create soft-links for neovim libs"
+
+    sudo ln -sf /opt/nvim-linux64/share/* /usr/local/share/man/
+    check_error "Failed to create soft-links for neovim libs"
+
+    sudo ln -sf /opt/nvim-linux64/share/* /usr/local/share/
+    check_error "Failed to create soft-links for neovim libs"
+fi
