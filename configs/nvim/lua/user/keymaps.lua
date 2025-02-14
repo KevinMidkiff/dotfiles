5cdccd785ffec5338f1eab0beb17470b8c90b308
Kevin Midkiff <midkiff.km@gmail.com>
2025-02-14 15:09:48 -0800

    Fix broken configuration in neovim and add variable for tmux window color

--- configs/nvim/lua/user/keymaps.lua
+++ configs/nvim/lua/user/keymaps.lua
@@ -46,6 +46,3 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 keymap("n", "<leader>ff", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
 keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
 keymap("n", "<leader>fb", "<cmd>lua require'telescope.builtin'.buffers(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
-
--- Nvimtree
-keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
