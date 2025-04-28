7dbbcc6d2db1147b0c0cfdc78b5c7daf1a0a163e
Kevin Midkiff <midkiff.km@gmail.com>
2025-04-28 16:27:34 -0700

    Add keymap for live grep

--- configs/nvim/lua/user/keymaps.lua
+++ configs/nvim/lua/user/keymaps.lua
@@ -44,7 +44,7 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 -- Telescope --
 -- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
 keymap("n", "<leader>ff", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
-keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
+keymap("n", "<leader>tt", "<cmd>Telescope live_grep<cr>", opts)
 keymap("n", "<leader>fb", "<cmd>lua require'telescope.builtin'.buffers(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
 
 keymap("n", "<leader>vs", '<cmd>VenvSelect<cr>', opts)
